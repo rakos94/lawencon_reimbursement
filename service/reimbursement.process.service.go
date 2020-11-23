a46bdf82b3b938a8fd67cb093133bea91249de20
kamal <rakos94@gmail.com>
2020-11-23 17:40:49 +0700

    Add reimbursement process

---
+++ service/reimbursement.process.service.go
@@ -0,0 +1,12 @@
+package service
+
+import "lawencon/reimbursement/model"
+
+// ReimbursementProcessService ...
+type ReimbursementProcessService interface {
+	CreateReimbursementProcess(data *model.ReimbursementProcess) (*model.ReimbursementProcess, error)
+	GetReimbursementProcessAll() ([]model.ReimbursementProcess, error)
+	GetReimbursementProcessByID(id string) (model.ReimbursementProcess, error)
+	GetReimbursementProcessByReimbursementID(id string) (model.ReimbursementProcess, error)
+	UpdateReimbursementProcess(id string, data *model.ReimbursementProcess) (*model.ReimbursementProcess, error)
+}
