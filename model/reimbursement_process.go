270a35d05cefdcdb1e7dbeb2b4181e4233012424
kamal <rakos94@gmail.com>
2020-11-23 16:11:05 +0700

    Update datatype date

--- model/reimbursement_process.go
+++ model/reimbursement_process.go
@@ -1,13 +1,15 @@
 package model
 
+import "lawencon/reimbursement/datatype"
+
 // ReimbursementProcess ...
 type ReimbursementProcess struct {
 	BaseModels
-	ReimbursementID string `gorm:"not null"`
-	PeriodStart     string `gorm:"not null;type:date"`
-	PeriodEnd       string `gorm:"not null;type:date"`
-	PaidDate        string `gorm:"not null;type:date"`
-	PaidType        string `gorm:"not null;type:date"`
+	ReimbursementID string        `gorm:"not null"`
+	PeriodStart     datatype.Date `gorm:"not null;type:date"`
+	PeriodEnd       datatype.Date `gorm:"not null;type:date"`
+	PaidDate        datatype.Date `gorm:"not null;type:date"`
+	PaidType        string        `gorm:"not null;type:date"`
 	BaseCUModels
 }
 
