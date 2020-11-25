5040ab57d7148144e0af6e7f3a0a230061ae4ad3
kamal <rakos94@gmail.com>
2020-11-25 10:03:56 +0700

    Add unique index

--- model/reimbursement_request_status.go
+++ model/reimbursement_request_status.go
@@ -2,9 +2,10 @@ package model
 
 type ReimbursementRequestStatus struct {
 	BaseModels
-	StatusCode	string `gorm:"column:status_code; not null" json:"status_code"`
-	Name 		string `gorm:"column:name; not null" json:"name"`
+	StatusCode string `gorm:"column:status_code; not null;uniqueIndex" json:"status_code"`
+	Name       string `gorm:"column:name; not null" json:"name"`
 }
-func (ReimbursementRequestStatus)TableName()string  {
+
+func (ReimbursementRequestStatus) TableName() string {
 	return "reimbursement_request_status"
 }
