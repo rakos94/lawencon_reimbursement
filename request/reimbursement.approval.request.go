658a32d9beb7e143ada0b13607e06bfd326725c0
kamal <rakos94@gmail.com>
2020-11-25 15:53:19 +0700

    Update nullable request

--- request/reimbursement.approval.request.go
+++ request/reimbursement.approval.request.go
@@ -1,10 +1,13 @@
 package request
 
+import "lawencon/reimbursement/datatype"
+
 // ReimbursementApprovedRequest ...
 type ReimbursementApprovedRequest struct {
-	ReimbursementID string  `json:"reimbursement_id" validate:"required"`
-	Reason          string  `json:"reason" validate:"required"`
-	StatusCode      string  `json:"status_code" validate:"required"`
-	PaidType        string  `json:"paid_type" validate:"required"`
-	ApprovedAmount  float64 `json:"approved_amount" validate:"required"`
+	ReimbursementProcessID string         `json:"reimbursement_process_id" validate:"required"`
+	Reason                 string         `json:"reason" validate:"required"`
+	StatusCode             string         `json:"status_code" validate:"required"`
+	PaidDate               *datatype.Date `json:"paid_date"`
+	PaidType               *string        `json:"paid_type"`
+	ApprovedAmount         *float64       `json:"approved_amount"`
 }
