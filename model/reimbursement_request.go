1a8cddd33a53c96976ad29e113a17dfe59c16ff4
kamal <rakos94@gmail.com>
2020-11-25 11:17:55 +0700

    Rename some column

--- model/reimbursement_request.go
+++ model/reimbursement_request.go
@@ -2,14 +2,14 @@ package model
 
 type ReimbursementRequest struct {
 	BaseModels
-	EmployeeID        	string 	`gorm:"column:employee_id;not null" json:"employee_id"`
-	ReimbursementCode 	string 	`gorm:"column:reimbursement_code;not null" json:"reimbursement_code"`
-	TrxCode				string 	`gorm:"column:trx_code;not null" json:"trx_code"`
-	TrxDate				string 	`gorm:"column:trx_date;not null" json:"trx_date"`
-	StatusCode			string 	`gorm:"column:status_code:not null" json:"status_code"`
-	IsPaid				bool	`gorm:"column:is_paid:not null" json:"is_paid"`
+	EmployeeID            string `gorm:"column:employee_id;not null" json:"employee_id"`
+	ReimbursementTypeCode string `gorm:"column:reimbursement_code;not null" json:"reimbursement_type_code"`
+	TrxCode               string `gorm:"column:trx_code;not null" json:"trx_code"`
+	TrxDate               string `gorm:"column:trx_date;not null" json:"trx_date"`
+	StatusCode            string `gorm:"column:status_code:not null" json:"status_code"`
+	IsPaid                bool   `gorm:"column:is_paid:not null" json:"is_paid"`
 }
 
-func (ReimbursementRequest)TableName()string  {
+func (ReimbursementRequest) TableName() string {
 	return "reimbursement_request"
 }
--- model/reimbursement_request_detail.go
+++ model/reimbursement_request_detail.go
@@ -2,12 +2,12 @@ package model
 
 type ReimbursementRequestDetail struct {
 	BaseModels
-	Reciver              string               `gorm:"column:reciver" json:"reciver"`
-	Description          string               `gorm:"column:description" json:"description"`
-	Attachment           string               `gorm:"column:attachment" json:"attachment"`
-	TotalAmount          float64              `gorm:"column:total_amount" json:"total_amount"`
-	ReimbursementId      string               `gorm:"column:reimbursement_id" json:"reimbursement_id"`
-	ReimbursementRequest ReimbursementRequest `gorm:"foreignKey:reimbursement_id" json:"reimbursement_request"`
+	Reciver                string               `gorm:"column:reciver" json:"reciver"`
+	Description            string               `gorm:"column:description" json:"description"`
+	Attachment             string               `gorm:"column:attachment" json:"attachment"`
+	TotalAmount            float64              `gorm:"column:total_amount" json:"total_amount"`
+	ReimbursementRequestID string               `gorm:"column:reimbursement_request_id" json:"reimbursement_request_id"`
+	ReimbursementRequest   ReimbursementRequest `gorm:"foreignKey:reimbursement_request_id" json:"reimbursement_request"`
 }
 
 func (ReimbursementRequestDetail) TableName() string {
