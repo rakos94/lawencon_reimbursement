bd22f6da88bbadf075f1e53e6e1d34503b0cc78f
kamal <rakos94@gmail.com>
2020-11-25 17:57:46 +0700

    Fix spacing

--- service/reimbursementRequest.service.impl.go
+++ service/reimbursementRequest.service.impl.go
@@ -5,23 +5,24 @@ import (
 	"lawencon/reimbursement/model"
 )
 
-var reimbursementDao dao.ReimbursementRequestDao =dao.ReimbursementRequestDaoImpl{}
-type ReimbursementRequestServiceimpl struct {}
+var reimbursementDao dao.ReimbursementRequestDao = dao.ReimbursementRequestDaoImpl{}
 
-func (ReimbursementRequestServiceimpl)CreateReimbursementRequest(data * model.ReimbursementRequest) error  {
-	return  reimbursementDao.CreateReimbursementRequest(data)
+type ReimbursementRequestServiceimpl struct{}
+
+func (ReimbursementRequestServiceimpl) CreateReimbursementRequest(data *model.ReimbursementRequest) error {
+	return reimbursementDao.CreateReimbursementRequest(data)
 }
-func (ReimbursementRequestServiceimpl)GetAllReimbursementRequest()([]model.ReimbursementRequest,error)  {
-	return reimbursementDao.GetAllReimbursementRequest();
+func (ReimbursementRequestServiceimpl) GetAllReimbursementRequest() ([]model.ReimbursementRequest, error) {
+	return reimbursementDao.GetAllReimbursementRequest()
 }
 
-func (ReimbursementRequestServiceimpl)GetByIdReimbursementRequest(id string)(model.ReimbursementRequest,error)  {
+func (ReimbursementRequestServiceimpl) GetByIdReimbursementRequest(id string) (model.ReimbursementRequest, error) {
 	return reimbursementDao.GetByIdReimbursementRequest(id)
 }
-func (ReimbursementRequestServiceimpl)UpdateReimbursementRequest(id string, data * model.ReimbursementRequest)error  {
-	return reimbursementDao.UpdateReimbursementRequest(id,data)
+func (ReimbursementRequestServiceimpl) UpdateReimbursementRequest(id string, data *model.ReimbursementRequest) error {
+	return reimbursementDao.UpdateReimbursementRequest(id, data)
 }
 
-func (ReimbursementRequestServiceimpl)DeleteReimbursementRequest(id string)error  {
+func (ReimbursementRequestServiceimpl) DeleteReimbursementRequest(id string) error {
 	return reimbursementDao.DeleteReimbursementRequest(id)
 }
