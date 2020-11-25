91f4a014d146ccbd477325eb288c4025f34be9e6
kamal <rakos94@gmail.com>
2020-11-25 13:45:23 +0700

    Remove type date at PaidType

--- model/reimbursement_process_paid.go
+++ model/reimbursement_process_paid.go
@@ -7,7 +7,7 @@ type ReimbursementProcessPaid struct {
 	BaseModels
 	ReimbursementProcessID string        `gorm:"not null" json:"reimbursement_process_id"`
 	PaidDate               datatype.Date `gorm:"not null;type:date" json:"paid_date"`
-	PaidType               string        `gorm:"not null;type:date" json:"paid_type"`
+	PaidType               string        `gorm:"not null" json:"paid_type"`
 	ApprovedAmount         float64       `gorm:"not null" json:"approved_amount"`
 	BaseCUModels
 }
