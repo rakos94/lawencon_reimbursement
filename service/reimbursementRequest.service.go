05304ca42887f69d444f032d1f3d2f3217711879
kamal <rakos94@gmail.com>
2020-11-25 17:57:10 +0700

    Fix typo

--- service/reimbursementRequest.service.go
+++ service/reimbursementRequest.service.go
@@ -3,9 +3,9 @@ package service
 import "lawencon/reimbursement/model"
 
 type ReimbursementRequestService interface {
-	CreateReimbursementRequest(data * model.ReimbursementRequest) error
-	GetAllReimbursementRequest()([]model.ReimbursementRequest,error)
-	GetByIdReimbursementRequest(id string)(model.ReimbursementRequest,error)
-	UpdateReimbursementRequest(id string, data * model.ReimbursementRequest)error
-	DeleteReimbursementRequest(id string)error
+	CreateReimbursementRequest(data *model.ReimbursementRequest) error
+	GetAllReimbursementRequest() ([]model.ReimbursementRequest, error)
+	GetByIdReimbursementRequest(id string) (model.ReimbursementRequest, error)
+	UpdateReimbursementRequest(id string, data *model.ReimbursementRequest) error
+	DeleteReimbursementRequest(id string) error
 }
