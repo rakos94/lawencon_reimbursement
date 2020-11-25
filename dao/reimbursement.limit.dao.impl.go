6db94818978e56b4227bf1f5599c101ee6581e6d
kamal <rakos94@gmail.com>
2020-11-25 12:26:12 +0700

    Add validate limit already exist or not

--- dao/reimbursement.limit.dao.impl.go
+++ dao/reimbursement.limit.dao.impl.go
@@ -1,12 +1,20 @@
 package dao
 
-import "lawencon/reimbursement/model"
+import (
+	"errors"
+	"lawencon/reimbursement/model"
+)
 
 // ReimbursementLimitDaoImpl ...
 type ReimbursementLimitDaoImpl struct{}
 
 // CreateReimbursementLimit ...
 func (ReimbursementLimitDaoImpl) CreateReimbursementLimit(data *model.ReimbursementLimit) (*model.ReimbursementLimit, error) {
+	err := checkExistEmployeeIDAndTypeCode(data)
+	if err != nil {
+		return nil, err
+	}
+
 	result := g.Create(&data)
 	if result.Error != nil {
 		return nil, result.Error
@@ -51,9 +59,34 @@ func (ReimbursementLimitDaoImpl) UpdateReimbursementLimit(id string, data *model
 	if first.Error != nil {
 		return m, first.Error
 	}
+
+	// check employee already have new ReimbursementTypeCode or not
+	if data.ReimbursementTypeCode != m.ReimbursementTypeCode {
+		data.EmployeeID = m.EmployeeID
+		err := checkExistEmployeeIDAndTypeCode(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	result := g.Model(m).Updates(data)
 	if result.Error != nil {
 		return m, result.Error
 	}
 	return m, nil
+}
+
+func checkExistEmployeeIDAndTypeCode(data *model.ReimbursementLimit) error {
+	var count int64
+	m := &model.ReimbursementLimit{}
+	result := g.Model(m).Where("employee_id", data.EmployeeID).
+		Where("reimbursement_type_code", data.ReimbursementTypeCode).
+		Count(&count)
+	if result.Error != nil {
+		return result.Error
+	}
+	if count > 0 {
+		return errors.New("Employee id & Reimbursement type code already exist")
+	}
+	return nil
 }
