514b044abc9af7a624f3891ffdf9e94c64ac4e44
kamal <rakos94@gmail.com>
2020-11-25 09:10:26 +0700

    Add cli handle, seeder

--- main.go
+++ main.go
@@ -1,38 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"github.com/labstack/echo"
 	"lawencon/reimbursement/config"
 	"lawencon/reimbursement/controller"
 	"lawencon/reimbursement/dao"
 	"lawencon/reimbursement/service"
+	"log"
+	"os"
+
+	"github.com/labstack/echo"
 
 	"gorm.io/gorm"
 )
 
 func main() {
-	e:= echo.New()
+	e := echo.New()
 	g := NewConn()
-	//config.Conn();
+
 	dao.SetDao(g)
+
+	// CLI HANDLE
+	if len(os.Args) > 1 {
+		handleArgs()
+		return
+	}
+
 	service.SetService(g)
 
 	jwtGroup := config.SetJwt(e)
 	config.ClientConnect()
 	controller.SetReimbursemetRequest(jwtGroup)
+	controller.SetReimbursemetCategory(jwtGroup)
 	e.Logger.Fatal(e.Start(":" + config.AppPort))
 }
 
 // NewConn ...
 func NewConn() *gorm.DB {
 	g, err := config.Conn()
-
-
-	if err == nil {
-		config.MigrateSchema(g)
-		fmt.Println("Success Create Table")
+	if err != nil {
+		log.Println(err)
 	}
 
 	return g
+}
+
+func handleArgs() {
+	arg := os.Args[1]
+
+	if arg == "seeder" {
+		config.SeederRun()
+	} else if arg == "migrate" {
+		migrateTable()
+	} else if arg == "migrate:seed" {
+		migrateTable()
+		config.SeederRun()
+	}
+}
+
+func migrateTable() {
+	g := NewConn()
+	err := config.MigrateSchema(g)
+	if err != nil {
+		log.Println(err.Error())
+	} else {
+		log.Println("Success Create Table")
+	}
 }
