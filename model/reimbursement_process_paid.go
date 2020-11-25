3306e52492cfbdb6483e38be8488efb6e9ecd968
kamal <rakos94@gmail.com>
2020-11-25 13:08:52 +0700

    Add json annotation

--- model/reimbursement_process_paid.go
+++ model/reimbursement_process_paid.go
@@ -5,10 +5,10 @@ import "lawencon/reimbursement/datatype"
 // ReimbursementProcessPaid ...
 type ReimbursementProcessPaid struct {
 	BaseModels
-	ReimbursementProcessID string        `gorm:"not null"`
-	PaidDate               datatype.Date `gorm:"not null;type:date"`
-	PaidType               string        `gorm:"not null;type:date"`
-	ApprovedAmount         float64       `gorm:"not null"`
+	ReimbursementProcessID string        `gorm:"not null" json:"reimbursement_process_id"`
+	PaidDate               datatype.Date `gorm:"not null;type:date" json:"paid_date"`
+	PaidType               string        `gorm:"not null;type:date" json:"paid_type"`
+	ApprovedAmount         float64       `gorm:"not null" json:"approved_amount"`
 	BaseCUModels
 }
 
